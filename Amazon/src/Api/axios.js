24261e12f21cab367446ad3e523e0652d4799575
sami692139 <samuel.tassew2020@gmail.com>
2024-11-28 00:43:28 +0300

    change

--- Amazon/src/Api/axios.js
+++ Amazon/src/Api/axios.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:5001/clon-6eb90/us-central1/api",
+  //localhost baseurl
+  // baseURL: "http://127.0.0.1:5001/clon-6eb90/us-central1/api",
+//deployed version of amazon server on render.com
+  baseURL: "https://amazon-api-deploy-9g1t.onrender.com/",
 });
 export {axiosInstance}
